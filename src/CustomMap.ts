8c68335f4009c2e87f2a8069df37ebb80bf6387d
Trace Suiter <t.suit@utexas.edu>
2021-11-15 23:35:55 -0600

    Created addMarker function and added an interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,15 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// Instructions to every other class on how they
+// can be an argument to 'addMarker'
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -10,4 +22,37 @@ export class CustomMap {
             }
     });
 }
+
+addMarker(mappable: Mappable): void {
+    mappable
+    new this.googleMap.maps.Marker({
+        map: this.googleMap,
+        position: {
+            lat: mappable.location.lat,
+            lng: mappable.location.lng
+        }
+    });
+}
+
+// This is a bad approach
+// addMarker(mappable: User | Company): void {
+//     mappable
+//     new this.googleMap.maps.Marker({
+//         map: this.googleMap,
+//         position: {
+//             lat: mappable.location.lat,
+//             lng: mappable.location.lng
+//         }
+//     });
+// }
+
+// addCompanyMarker(company: Company): void {
+//     new this.googleMap.maps.Marker({
+//         map: this.googleMap,
+//         position: {
+//             lat: company.location.lat,
+//             lng: company.location.lng
+//         }
+//     });
+// }
 }
